137d79cfcdf86784c94cf1860e863d446b250a85
Emrah Diril <emrah.diril@fivetran.com>
2024-03-22 01:44:39 -0400

    Add check for key is in configuration (#13)

--- examples/configuration/connector.py
+++ examples/configuration/connector.py
@@ -1,9 +1,22 @@
 from fivetran_customer_sdk import Connector
 from fivetran_customer_sdk import Operations as op
 from cryptography.fernet import Fernet
+import json
 
 
 encrypted_message = b'gAAAAABl-3QGKUHpdUhBNpnW1_SnSkQGrAwev-uBBJaZo4NmtylIMg8UX6usuG4Z-h80OvfJajW6HU56O5hofapEIh4W33vuMpJgq0q3qMQx6R3Ol4qZ3Wc2DyIIapxbK5BrQHshBF95'
+
+
+def schema(configuration: dict):
+    if 'my_key' not in configuration:
+        raise ValueError("Could not find 'my_key'")
+
+    return [
+            {
+                "table": "crypto",
+                "primary_key": ["msg"],
+            }
+    ]
 
 
 def update(configuration: dict, state: dict):
@@ -12,13 +25,16 @@ def update(configuration: dict, state: dict):
 
     message = f.decrypt(encrypted_message)
 
-    yield op.upsert(table="crypto", data={'msg': message})
+    yield op.upsert(table="crypto", data={
+        'msg': message.decode()
+    })
 
 
-connector = Connector(update=update)
+connector = Connector(update=update, schema=schema)
 
 
 if __name__ == "__main__":
-    connector.debug(configuration={
-        'my_key': 'EKlFpH8sZmdhhZ9lGhezgMTwAw3_Y2e7wbco7Gxt3SA='
-    })
+    with open("configuration.json", 'r') as f:
+        configuration = json.load(f)
+    connector.debug(configuration=configuration)
+
