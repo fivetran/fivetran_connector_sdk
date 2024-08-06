c539ebac12453cfee66c1655f724e4bfb33d01de
Niket Khandelwal <niket.khandelwal@fivetran.com>
2024-08-06 15:13:23 +0530

    call out logging in example

--- examples/weather/connector.py
+++ examples/weather/connector.py
@@ -1,6 +1,6 @@
 # This is a simple example for how to work with the fivetran_connector_sdk module.
-# It shows the use of a requirements.txt file and a connector that calls a publicly available API
-# to get the weather forecast data for Myrtle Beach in South Carolina, USA.
+# It shows the use of a requirements.txt file and a connector that calls a publicly available API to get the weather forecast data for Myrtle Beach in South Carolina, USA.
+# It also shows how to use the logging functionality provided by fivetran_connector_sdk, by logging important steps using log.info() and log.fine()
 # See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
 # and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details.
 
