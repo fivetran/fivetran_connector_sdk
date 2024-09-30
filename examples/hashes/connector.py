1fe8a869eca535d74cc4bc8176308f423dae5bee
Rajat Kushwaha <rajat@fivetran.com>
2024-09-30 19:10:22 +0530

    Add two more rows

--- examples/hashes/connector.py
+++ examples/hashes/connector.py
@@ -45,36 +45,60 @@ def update(configuration: dict, state: dict):
 
     # Represents a record fetched from source
     row_1 = {
-        "first_name": "John",  # First name.
-        "last_name": "Doe",  # Last name.
-        "email": "john.doe@example.com",  # Email ID.
-        "updated_at": "2007-12-03T10:15:30Z"  # Updated at timestamp.
+        "first_name": "John",  # First name
+        "last_name": "Doe",  # Last name
+        "email": "john.doe@example.com",  # Email ID
+        "updated_at": "2007-12-03T10:15:30Z"  # Updated at timestamp
     }
 
     # Generate hash and add this as a key in the dictionary
     row_1["hash_id"] = generate_row_hash(row_1)
 
-    # This source record has None for the email field
+    # Represents the same record fetched from source with a different updated_at timestamp
+    row_11 = {
+        "first_name": "John",  # First name
+        "last_name": "Doe",  # Last name
+        "email": "john.doe@example.com",  # Email ID
+        "updated_at": "2008-02-13T03:30:50Z"  # Updated at timestamp changed
+    }
+
+    # Generate hash and add this as a key in the dictionary
+    row_11["hash_id"] = generate_row_hash(row_11)
+
+    # Represents the same record fetched from source but this time email is null
+    row_12 = {
+        "first_name": "John",  # First name
+        "last_name": "Doe",  # Last name
+        "email": None,  # Email ID
+        "updated_at": "2008-02-13T03:30:50Z"  # Updated at timestamp remains same
+    }
+
+    # Generate hash and add this as a key in the dictionary
+    row_12["hash_id"] = generate_row_hash(row_12)
+
+    # Represents another record fetched from source. This has null for the email field
     row_2 = {
-        "first_name": "Joe",  # First name.
-        "last_name": "Smith",  # Last name.
-        "email": None,  # Email ID.
-        "updated_at": "2014-05-10T00:00:30Z"  # Updated at timestamp.
+        "first_name": "Joe",  # First name
+        "last_name": "Smith",  # Last name
+        "email": None,  # Email ID
+        "updated_at": "2014-05-10T00:00:30Z"  # Updated at timestamp
     }
 
     row_2["hash_id"] = generate_row_hash(row_2)
 
-    # This source record has the field email missing in the source
+    # Represents another record fetched from source. This has the field email missing
     row_3 = {
-        "first_name": "Jane",  # First name.
-        "last_name": "Dalton",  # Last name.
-        "updated_at": None  # Updated at timestamp.
+        "first_name": "Jane",  # First name
+        "last_name": "Dalton",  # Last name
+        "updated_at": None  # Updated at timestamp
     }
 
     row_3["hash_id"] = generate_row_hash(row_3)
 
     # Yield an upsert operation to insert/update the row in the "user" table.
     yield op.upsert(table="user", data=row_1)
+    yield op.upsert(table="user", data=row_11)
+    yield op.upsert(table="user", data=row_12)
     yield op.upsert(table="user", data=row_2)
     yield op.upsert(table="user", data=row_3)
 
@@ -110,6 +134,8 @@ if __name__ == "__main__":
 # │                     varchar                 │     varchar       │      varchar      │      varchar           |  timestamp with time zone     │
 # ├─────────────────────────────────────────────┼───────────────────┼───────────────────┼────────────────────────┤───────────────────────────────│
 # │    9507eb591ddb60eb68452d06cf70696d8d5e8140 │       John        │        Doe        │ john.doe@example.com   │ 2007-12-03 10:15:30.000 +0000 │
+# │    7d09ec9c2cfa02549a329a36fcd0221b1ea020ab │       John        │        Doe        │ john.doe@example.com   │ 2008-02-13 03:30:50.000 +0000 │
+# │    f01ca16d545bf1af5a8bbce87acc622101e1a992 │       John        │        Doe        │         [NULL]         │ 2008-02-13 03:30:50.000 +0000 │
 # │    a211fe507efd31c94e30e91fc8883b4a68605d3d │       Joe         │       Smith       │         [NULL]         │ 2014-05-10 00:00:30.000 +0000 │
 # │    aa1f248cbe6e71e1f36db2f09c5b68a2fbe3febb │       Jane        │       Dalton      │         [NULL]         │             [NULL]            │
 # └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
