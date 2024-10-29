3c285a74b11997b1f2b9b8da17b5985ed1cf0c01
SatvikPatil <54502802+fivetran-satvikpatil@users.noreply.github.com>
2024-10-29 19:06:05 +0530

    fix(User Profiles Example): Handle NaN value from dataframes (#44)
    * NaN handling
    
    * formatting
    
    * separate the approaches into the methods
    
    * requirements change
    
    * add python comments
    
    * comment change
    
    ---------
    
    Co-authored-by: Satvik Patil <satvik.patil@satvik.patil-W4LTPGX1Q0>

--- examples/quickstart_examples/user_profiles/connector.py
+++ examples/quickstart_examples/user_profiles/connector.py
@@ -1,4 +1,7 @@
 # This is a simple example for how to work with the fivetran_connector_sdk module.
+# This example demonstrates how to work with data frames.
+# It also shows how to handle NaN values, as we do not support NaN inputs.
+# Customers should convert NaN values to None before sending data.
 # It shows the use of a requirements.txt file and a connector that calls a publicly available API to get random profile data
 # See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
 # and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details
@@ -10,6 +13,7 @@ from fivetran_connector_sdk import Logging as log
 # Import the requests module for making HTTP requests, aliased as rq.
 import requests as rq
 import pandas as pd
+import numpy as np
 
 # Define the schema function which lets you configure the schema your connector delivers.
 # See the technical reference documentation for more details on the schema function:
@@ -34,6 +38,10 @@ def schema(configuration: dict):
             "table": "login",
             "primary_key": ["profileId"],
             # Columns and data types will be inferred by Fivetran
+        },
+        {
+            "table": "table_with_nan",
+            "primary_key": ["id"],
         }
     ]
 
@@ -56,38 +64,14 @@ def update(configuration: dict, state: dict):
     profile_df, location_df, login_df, cursor = get_data(profile_cursor)
 
     # Approaches to upsert the records from dataFrame
+    yield from upsert_dataframe_approach_1(profile_df=profile_df, state=state, cursor=cursor)
+    
+    yield from upsert_dataframe_approach_2(location_df=location_df, state=state, cursor=cursor)
 
-    # APPROACH 1: Gives you direct access to individual row values by column name, Slower approach, helpful for custom row handling
-    # UPSERT all profile table data, checkpoint periodically to save progress. In this example every 5 records.
-    for index, row in profile_df.iterrows():
-        yield op.upsert("profile", {col: row[col] for col in profile_df.columns})
-        if index % 5 == 0:
-            state["profile_cursor"] = row["id"]
-            yield op.checkpoint(state)
+    yield from upsert_dataframe_approach_3(login_df=login_df, state=state, cursor=cursor)
 
-    # Checkpointing at the end of the "profile" table data processing
-    state["profile_cursor"] = cursor
-    yield op.checkpoint(state)
-
-    # APPROACH 2: Generally faster and more memory-efficient, Simplifies the code since rows are already dictionaries and can be used directly
-    # UPSERT all location table data.
-    # Iterate over each row in the DataFrame, converting it to a dictionary
-    for row in location_df.to_dict("records"):
-        yield op.upsert("location", row)
-
-    # Checkpointing at the end of the "location" table data processing
-    state["location_cursor"] = cursor
-    yield op.checkpoint(state)
-
-    # APPROACH 3: Faster approach, Keeps track of the original indices of the rows, which can be useful for certain operations that require indexing information.
-    # UPSERT all login table data.
-    # Iterate over the values of the dictionary (which are the DataFrame rows)
-    for value in login_df.to_dict("index").values():
-        yield op.upsert("login", value)
-
-    # Checkpointing at the end of the "login" table data processing
-    state["login_cursor"] = cursor
-    yield op.checkpoint(state)
+    # Approaches to handle NaN values in dataframes
+    yield from handle_tables_with_nan(state)
 
 
 # Function to fetch data from an API and process it into DataFrames
@@ -97,7 +81,7 @@ def get_data(cursor):
     location_df = pd.DataFrame([])
     login_df = pd.DataFrame([])
 
-    # Fetch data for 10 iterations, each with a new profile
+    # Fetch data for 3 iterations, each with a new profile
     for i in range(2):
         # Increment primary key
         cursor += 1
@@ -148,6 +132,84 @@ def get_data(cursor):
             [login_df, pd.DataFrame([login_data])], ignore_index=True)
 
     return profile_df, location_df, login_df, cursor
+
+def handle_tables_with_nan(state: dict):
+    # Upsert tables with NaN
+    table_df_1 = generate_data_with_NaN()
+    table_df_2 = generate_data_with_NaN()
+    table_df_3 = generate_data_with_NaN()
+
+    # Approaches to replace NaN with None
+
+    # Approach 1: Convert NaN to None using the replace method
+    table_df_1 = table_df_1.replace(np.nan, None)
+    yield from upsert_dataframe(table_df=table_df_1, state=state)
+
+    # Approach 2: Convert NaN to None using the where method
+    table_df_2 = table_df_2.astype(object).where(pd.notnull(table_df_2), None)
+    yield from upsert_dataframe(table_df=table_df_2, state=state)
+
+    # Approach 3: Fill NaN values with np.nan and then replace any NaN with None.
+    table_df_3 = table_df_3.fillna(np.nan).replace([np.nan], [None])
+    yield from upsert_dataframe(table_df=table_df_3, state=state)
+
+
+def generate_data_with_NaN():
+    np.random.seed(0)
+
+    # Create a DataFrame with 10 rows and 3 columns of random numbers
+    data = np.random.rand(10, 3)
+
+    # Randomly assign NaN values (e.g., 10% NaN)
+    nan_mask = np.random.choice([True, False], size=data.shape, p=[0.1, 0.9])
+    data[nan_mask] = np.nan
+
+    # Create the DataFrame
+    return pd.DataFrame(data, columns=['id', 'column_1', 'Column_2'])
+
+def upsert_dataframe(table_df, state):
+    cursor = state["table_with_nan_cursor"] if "table_with_nan_cursor" in state else 0
+    for row in table_df.to_dict("records"):
+        yield op.upsert("table_with_nan", row)
+        cursor += 1
+
+    state["table_with_nan_cursor"] = cursor
+    yield op.checkpoint(state)
+
+def upsert_dataframe_approach_1(profile_df, state, cursor):
+    # APPROACH 1: Gives you direct access to individual row values by column name, Slower approach, helpful for custom row handling
+    # UPSERT all profile table data, checkpoint periodically to save progress. In this example every 5 records.
+    for index, row in profile_df.iterrows():
+        yield op.upsert("profile", {col: row[col] for col in profile_df.columns})
+        if index % 5 == 0:
+            state["profile_cursor"] = row["id"]
+            yield op.checkpoint(state)
+
+    # Checkpointing at the end of the "profile" table data processing
+    state["profile_cursor"] = cursor
+    yield op.checkpoint(state)
+
+def upsert_dataframe_approach_2(location_df, state, cursor):
+    # APPROACH 2: Generally faster and more memory-efficient, Simplifies the code since rows are already dictionaries and can be used directly
+    # UPSERT all location table data.
+    # Iterate over each row in the DataFrame, converting it to a dictionary
+    for row in location_df.to_dict("records"):
+        yield op.upsert("location", row)
+
+    # Checkpointing at the end of the "location" table data processing
+    state["location_cursor"] = cursor
+    yield op.checkpoint(state)
+
+def upsert_dataframe_approach_3(login_df, state, cursor):
+    # APPROACH 3: Faster approach, Keeps track of the original indices of the rows, which can be useful for certain operations that require indexing information.
+    # UPSERT all login table data.
+    # Iterate over the values of the dictionary (which are the DataFrame rows)
+    for value in login_df.to_dict("index").values():
+        yield op.upsert("login", value)
+
+    # Checkpointing at the end of the "login" table data processing
+    state["login_cursor"] = cursor
+    yield op.checkpoint(state)
 
 
 # This creates the connector object that will use the update and schema functions defined in this connector.py file.
