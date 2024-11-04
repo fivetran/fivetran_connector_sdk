c9267ce2f5956b93cdd82aba1bfcd8cbe0d3c919
fivetran-kirkvanarkel <kirk.vanarkel@fivetran.com>
2024-11-04 09:06:25 -0700

    simplifying comments and structure

--- examples/source_examples/smartsheets/connector.py
+++ examples/source_examples/smartsheets/connector.py
@@ -5,8 +5,8 @@ import requests
 import json
 from datetime import datetime, timezone
 
-# Note: This code is currently configured to hit the GET Sheets endpoint and get data from 1 pre-defined sheet in Smartsheets that does not need pagination
-# GET Sheets endpoint: https://smartsheet.redoc.ly/tag/sheets?_gl=1*7tmndg*_gcl_au*MTYyNTczMDAxLjE3MzA0ODYyNzA.*_ga*MTAzMjIzODc4LjE3MzA0ODYyNzA.*_ga_ZYH7XNXMZK*MTczMDQ4NjI3MC4xLjEuMTczMDQ4Njg3Mi41My4wLjA.#operation/getSheet
+# Note: This code is currently configured to hit the getSheet endpoint and get data from 1 pre-defined sheet in Smartsheets that does not need pagination
+# getSheet endpoint: https://smartsheet.redoc.ly/tag/sheets#operation/getSheet
 # Add Additional code in the update function to handle multiple sheets and/or pagination through sheets
 # Can also add code to extract from other endpoints as needed
 def schema(configuration: dict):
@@ -22,7 +22,7 @@ def update(configuration: dict, state: dict):
     # Smartsheets sheets API endpoint
     sheets_url = 'https://api.smartsheet.com/2.0/sheets/' 
     # Define api_key and sheet_id from configuration.json
-    # If needing multiple sheets, define the sheet_id variable as a list and loop through API call through yield op.upsert 
+    # If needing multiple sheets, define the sheet_id variable as a list and loop through API call until yield op.upsert 
     api_token = configuration.get('smartsheet_api_token')
     sheet_id = configuration.get('smartsheet_sheet_id')
 
@@ -58,7 +58,6 @@ def update(configuration: dict, state: dict):
         for cell in cells:
             column_name = column_mapping.get(cell.get('columnId'))
             row_data[column_name] = cell.get('value')
-        print(row_data)
         # Upsert row to given table
         yield op.upsert("smartsheet_table_name", row_data)
 
