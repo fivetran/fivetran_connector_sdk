8ca304030517b3f64e6c2a0ab09abb4eca931f1e
fivetran-kirkvanarkel <108479451+fivetran-kirkvanarkel@users.noreply.github.com>
2024-11-19 16:16:18 -0700

    Add more code comments in connector.py

--- examples/source_examples/smartsheets/connector.py
+++ examples/source_examples/smartsheets/connector.py
@@ -1,3 +1,12 @@
+# This is a simple example for how to work with the fivetran_connector_sdk module.
+# Note: This code is currently configured to hit the getSheet endpoint and get data from 1 pre-defined sheet in Smartsheets that does not need pagination
+# You will need to provide your own smartsheet credentials for this to work --> api_key variable in configuration.json
+# getSheet endpoint: https://smartsheet.redoc.ly/tag/sheets#operation/getSheet
+# Add Additional code in the update function to handle multiple sheets and/or pagination through sheets
+# Can also add code to extract from other endpoints as needed
+# See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
+# and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details
+
 from fivetran_connector_sdk import Connector
 from fivetran_connector_sdk import Operations as op
 from fivetran_connector_sdk import Logging as log
@@ -5,10 +14,11 @@ import requests
 import json
 from datetime import datetime, timezone
 
-# Note: This code is currently configured to hit the getSheet endpoint and get data from 1 pre-defined sheet in Smartsheets that does not need pagination
-# getSheet endpoint: https://smartsheet.redoc.ly/tag/sheets#operation/getSheet
-# Add Additional code in the update function to handle multiple sheets and/or pagination through sheets
-# Can also add code to extract from other endpoints as needed
+# Define the schema function which lets you configure the schema your connector delivers.
+# See the technical reference documentation for more details on the schema function:
+# https://fivetran.com/docs/connectors/connector-sdk/technical-reference#schema
+# The schema function takes one parameter:
+# - configuration: a dictionary that holds the configuration settings for the connector.
 def schema(configuration: dict):
     return [
         {
@@ -18,6 +28,13 @@ def schema(configuration: dict):
         }
     ]
 
+# Define the update function, which is a required function, and is called by Fivetran during each sync.
+# See the technical reference documentation for more details on the update function
+# https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update
+# The function takes two parameters:
+# - configuration: dictionary contains any secrets or payloads you configure when deploying the connector
+# - state: a dictionary contains whatever state you have chosen to checkpoint during the prior sync
+# The state dictionary is empty for the first sync or for any full re-sync
 def update(configuration: dict, state: dict):
     # Smartsheets sheets API endpoint
     sheets_url = 'https://api.smartsheet.com/2.0/sheets/' 
@@ -64,9 +81,16 @@ def update(configuration: dict, state: dict):
     # Set cursor/filter time for next sync to be the start time of this current sync
     yield op.checkpoint(state = {"sync_cursor": sync_start})
 
+# This creates the connector object that will use the update function defined in this connector.py file.
 connector = Connector(schema=schema, update=update)
 
+# Check if the script is being run as the main module.
+# This is Python's standard entry method allowing your script to be run directly from the command line or IDE 'run' button.
+# This is useful for debugging while you write your code. Note this method is not called by Fivetran when executing your connector in production.
+# Please test using the Fivetran debug command prior to finalizing and deploying your connector.
 if __name__ == "__main__":
+    # Open the configuration.json file and load its contents into a dictionary.
     with open('configuration.json', 'r') as f:
         configuration = json.load(f)
+    # Adding this code to your `connector.py` allows you to test your connector by running your file directly from your IDE:
     connector.debug(configuration)
