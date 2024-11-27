a6b9469ef3cf0d99ce397272a62890a9a64f8552
Rishabh Ghosh <105623976+fivetran-rishabhghosh@users.noreply.github.com>
2024-11-27 11:18:53 +0530

    Update examples/source_examples/aws_athena/using_boto3/connector.py
    Co-authored-by: Alex Ilyichov <alexander.ilyichov@fivetran.com>

--- examples/source_examples/aws_athena/using_boto3/connector.py
+++ examples/source_examples/aws_athena/using_boto3/connector.py
@@ -119,7 +119,7 @@ connector = Connector(update=update, schema=schema)
 # Check if the script is being run as the main module.
 # This is Python's standard entry method allowing your script to be run directly from the command line or IDE 'run' button.
 # This is useful for debugging while you write your code. Note this method is not called by Fivetran when executing your connector in production.
-# Please test using the Fivetran debug command prior to finalizing and deploying your connector.
+# Test it by using the `debug` command prior to finalizing and deploying your connector.
 if __name__ == "__main__":
     # Open the configuration.json file and load its contents into a dictionary.
     with open("configuration.json", 'r') as f:
