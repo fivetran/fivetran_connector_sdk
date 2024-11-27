0d7021758f9ae96e3c6ef6c58254a2f86213ef09
Rishabh Ghosh <105623976+fivetran-rishabhghosh@users.noreply.github.com>
2024-11-27 11:18:28 +0530

    Update examples/source_examples/aws_athena/using_boto3/connector.py
    Co-authored-by: Alex Ilyichov <alexander.ilyichov@fivetran.com>

--- examples/source_examples/aws_athena/using_boto3/connector.py
+++ examples/source_examples/aws_athena/using_boto3/connector.py
@@ -80,7 +80,7 @@ def get_query_results(athena_client, query_execution_id):
 # See the technical reference documentation for more details on the update function
 # https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update
 # The function takes two parameters:
-# - configuration: dictionary contains any secrets or payloads you configure when deploying the connector
+# - configuration: a dictionary that contains any secrets or payloads you configure when deploying the connector
 # - state: a dictionary that contains whatever state you have chosen to checkpoint during the prior sync.
 # The state dictionary is empty for the first sync or for any full re-sync.
 def update(configuration: dict, state: dict):
