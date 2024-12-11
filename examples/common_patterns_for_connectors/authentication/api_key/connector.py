3d7e7957aff330eb6f565b050def7159cf567f34
Varun Dhall <varun@fivetran.com>
2024-12-11 22:47:48 +0530

    features(examples): added examples for Authentications  (#64)
    * feature(examples): added examples for http_basic and http_bearer
    
    * feature(examples): added api key auth example
    
    * feature(examples): added example for session token auth

---
+++ examples/common_patterns_for_connectors/authentication/api_key/connector.py
@@ -0,0 +1,148 @@
+# This is a simple example of how to work with API Key authentication for a REST API.
+# It defines a simple `update` method, which upserts retrieved data to a table named "user".
+# THIS EXAMPLE IS TO HELP YOU UNDERSTAND CONCEPTS USING DUMMY DATA. IT REQUIRES THE FIVETRAN-API-PLAYGROUND PACKAGE
+# (https://pypi.org/project/fivetran-api-playground/) TO RUN.
+# See the Technical Reference documentation
+# (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
+# and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details.
+
+# Import requests to make HTTP calls to API.
+import requests as rq
+# Import required classes from fivetran_connector_sdk.
+from fivetran_connector_sdk import Connector
+from fivetran_connector_sdk import Logging as log
+from fivetran_connector_sdk import Operations as op
+import json
+
+
+# Define the schema function which lets you configure the schema your connector delivers.
+# See the technical reference documentation for more details on the schema function:
+# https://fivetran.com/docs/connectors/connector-sdk/technical-reference#schema
+# The schema function takes one parameter:
+# - configuration: a dictionary that holds the configuration settings for the connector.
+def schema(configuration: dict):
+    return [
+        {
+            "table": "user",
+            "primary_key": ["id"],
+            "columns": {
+                "id": "STRING",
+                "name": "STRING",
+                "email": "STRING",
+                "address": "STRING",
+                "company": "STRING",
+                "job": "STRING",
+                "updatedAt": "UTC_DATETIME",
+                "createdAt": "UTC_DATETIME",
+            },
+        }
+    ]
+
+
+# Define the update function, which is a required function, and is called by Fivetran during each sync.
+# See the technical reference documentation for more details on the update function
+# https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update
+# The function takes two parameters:
+# - configuration: dictionary contains any secrets or payloads you configure when deploying the connector.
+# - state: a dictionary that contains whatever state you have chosen to checkpoint during the prior sync.
+# The state dictionary is empty for the first sync or for any full re-sync.
+def update(configuration: dict, state: dict):
+    log.warning("Example: Common Patterns For Connectors - Authentication - API KEY")
+
+    print("RECOMMENDATION: Please ensure the base url is properly set, you can also use "
+          "https://pypi.org/project/fivetran-api-playground/ to start mock API on your local machine.")
+    base_url = "http://127.0.0.1:5001/auth/api_key"
+
+    yield from sync_items(base_url, {}, state, configuration)
+
+
+# Define the get_auth_headers function, which is your custom function to generate auth headers for making API calls.
+# The function takes one parameter:
+# - config: dictionary contains any secrets or payloads you configure when deploying the connector.
+def get_auth_headers(config):
+    api_key = config.get('api_key')
+
+    if api_key is None:
+        raise ValueError("API Key is missing in the configuration.")
+
+    # Create the auth string
+    headers = {
+        "Authorization": f"apiKey {api_key}",
+        "Content-Type": "application/json",  # Optional: specify content type
+    }
+    return headers
+
+
+
+# The sync_items function handles the retrieval of API data.
+# It performs the following tasks:
+# 1. Sends an API request to the specified URL with the provided parameters.
+# 2. Processes the items returned in the API response by yielding upsert operations to Fivetran.
+# 3. Saves the state periodically to ensure the sync can resume from the correct point.
+#
+# The function takes three parameters:
+# - base_url: The URL to the API endpoint.
+# - params: A dictionary of query parameters to be sent with the API request.
+# - state: A dictionary representing the current state of the sync, including the last retrieved key.
+# - configuration: A dictionary contains any secrets or payloads you configure when deploying the connector.
+def sync_items(base_url, params, state, configuration):
+    response_page = get_api_response(base_url, params, get_auth_headers(configuration))
+
+    # Process the items.
+    items = response_page.get("data", [])
+    if not items:
+        return
+
+    # Iterate over each user in the 'items' list and yield an upsert operation.
+    # The 'upsert' operation inserts the data into the destination.
+    for user in items:
+        yield op.upsert(table="user", data=user)
+
+    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
+    # from the correct position in case of interruptions.
+    yield op.checkpoint(state)
+
+# The get_api_response function sends an HTTP GET request to the provided URL with the specified parameters.
+# It performs the following tasks:
+# 1. Logs the URL and query parameters used for the API call for debugging and tracking purposes.
+# 2. Makes the API request using the 'requests' library, passing the URL and parameters.
+# 3. Parses the JSON response from the API and returns it as a dictionary.
+#
+# The function takes two parameters:
+# - base_url: The URL to which the API request is made.
+# - params: A dictionary of query parameters to be included in the API request.
+#
+# Returns:
+# - response_page: A dictionary containing the parsed JSON response from the API.
+def get_api_response(base_url, params, headers):
+    log.info(f"Making API call to url: {base_url} with params: {params} and headers: {headers}")
+    response = rq.get(base_url, params=params, headers=headers)
+    response.raise_for_status()  # Ensure we raise an exception for HTTP errors.
+    response_page = response.json()
+    return response_page
+
+
+# This creates the connector object that will use the update and schema functions defined in this connector.py file.
+connector = Connector(update=update, schema=schema)
+
+# Check if the script is being run as the main module. This is Python's standard entry method allowing your script to
+# be run directly from the command line or IDE 'run' button. This is useful for debugging while you write your code.
+# Note this method is not called by Fivetran when executing your connector in production. Please test using the
+# Fivetran debug command prior to finalizing and deploying your connector.
+if __name__ == "__main__":
+    # Open the configuration.json file and load its contents into a dictionary.
+    with open("configuration.json", 'r') as f:
+        configuration = json.load(f)
+    # Adding this code to your `connector.py` allows you to test your connector by running your file directly from your IDE:
+    connector.debug(configuration=configuration)
+
+# Resulting table:
+# ┌───────────────────────────────────────┬───────────────┬────────────────────────┬──────────────────────────┬───────────────────────────┐
+# │                 id                    │      name     │         job            │        updatedAt         │        createdAt          │
+# │               string                  │     string    │       string           │      timestamp with UTC  │      timestamp with UTC   │
+# ├───────────────────────────────────────┼───────────────┼────────────────────────┼──────────────────────────┼───────────────────────────┤
+# │ c8fda876-6869-4aae-b989-b514a8e45dc6  │ Mark Taylor   │   Pilot, airline       │ 2024-09-22T19:35:41Z     │ 2024-09-22T18:50:06Z      │
+# │ 3910cbb0-27d4-47f5-9003-a401338eff6e  │ Alan Taylor   │   Dispensing optician  │ 2024-09-22T20:28:11Z     │ 2024-09-22T19:58:38Z      │
+# ├───────────────────────────────────────┴───────────────┴────────────────────────┴──────────────────────────┴───────────────────────────┤
+# │  2 rows                                                                                                                     5 columns │
+# └───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
