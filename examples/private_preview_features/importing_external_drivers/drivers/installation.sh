37f099b70b06e9e9b6637aa08989805addd08927
Pranav Totala <pranav.totala@fivetran.com>
2025-04-01 15:16:00 +0530

    Adressed PR Comments

--- examples/private_preview_features/importing_external_drivers/drivers/installation.sh
+++ examples/private_preview_features/importing_external_drivers/drivers/installation.sh
@@ -4,24 +4,14 @@
 
 # Detect OS
 OS=$(uname -s)
-DISTRO=$(awk -F= '$1=="NAME" {print $2}' /etc/os-release | tr -d '"')
+# DISTRO=$(awk -F= '$1=="NAME" {print $2}' /etc/os-release | tr -d '"')
 
 echo '{"level":"INFO", "message": "Detected OS: '$OS'", "message-origin": "installation_sh"}'
-echo '{"level":"INFO", "message": "Detected Distribution: '$DISTRO'", "message-origin": "installation_sh"}'
 
 # Update package lists
 update_packages() {
   if [[ "$OS" == "Linux" ]]; then
-    if [[ "$DISTRO" == *"Debian"* || "$DISTRO" == *"Ubuntu"* ]]; then
       sudo apt-get update
-    elif [[ "$DISTRO" == *"CentOS"* || "$DISTRO" == *"RHEL"* ]]; then
-      sudo yum update -y
-    elif [[ "$DISTRO" == *"Fedora"* ]]; then
-      sudo dnf update -y
-    else
-      echo '{"level":"INFO", "message": "Unsupported Linux distribution.", "message-origin": "installation_sh"}'
-      exit 1
-    fi
   else
     echo '{"level":"INFO", "message": "Unsupported OS.", "message-origin": "installation_sh"}'
     exit 1
@@ -31,16 +21,7 @@ update_packages() {
 # Install libpq5
 install_libpq5() {
   if [[ "$OS" == "Linux" ]]; then
-    if [[ "$DISTRO" == *"Debian"* || "$DISTRO" == *"Ubuntu"* ]]; then
-      sudo apt-get install -y libpq5 libpq-dev
-    elif [[ "$DISTRO" == *"CentOS"* || "$DISTRO" == *"RHEL"* ]]; then
-      sudo yum install -y postgresql-libs postgresql-devel # This package provides libpq on RHEL/CentOS
-    elif [[ "$DISTRO" == *"Fedora"* ]]; then
-        sudo dnf install -y postgresql-libs postgresql-devel #This package provides libpq on Fedora
-    else
-      echo '{"level":"INFO", "message": "Unsupported Linux distribution.", "message-origin": "installation_sh"}'
-      exit 1
-    fi
+    sudo apt-get install -y libpq5 libpq-dev
   else
     echo '{"level":"INFO", "message": "Unsupported OS.", "message-origin": "installation_sh"}'
     exit 1
