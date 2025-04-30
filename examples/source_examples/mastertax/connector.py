90c98c378c02f58375fd69d394aef9a803b07aef
CL Abeel <89160012+fivetran-clgritton@users.noreply.github.com>
2025-04-30 10:01:55 -0400

    Update connector.py
    changed "main" to "__main__"

--- examples/source_examples/mastertax/connector.py
+++ examples/source_examples/mastertax/connector.py
@@ -266,7 +266,7 @@ connector = Connector(update=update, schema=schema)
 # This is Python's standard entry method allowing your script to be run directly from the command line or IDE 'run' button.
 # This is useful for debugging while you write your code. Note this method is not called by Fivetran when executing your connector in production.
 # Please test using the Fivetran debug command prior to finalizing and deploying your connector.
-if __name__ == "main":
+if __name__ == "__main__":
     # Open the configuration.json file and load its contents into a dictionary.
     with open("configuration.json", 'r') as f:
         configuration = json.load(f)
