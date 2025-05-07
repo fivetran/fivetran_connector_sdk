e732e3971c6150c520b8cb46c8712b9c678f42ce
Sahil Khirwal <sahil.khirwal@fivetran.com>
2025-05-07 14:09:10 +0530

    adding clickhouse example

---
+++ examples/source_examples/clickhouse/REAMDE.md
@@ -0,0 +1,85 @@
+# **ClickHouse Database Example**
+
+**Connector Overview**
+
+This connector shows how to pull data from ClickHouse databases. It connects to a specified ClickHouse instance, extracts data from tables, and syncs it to your destination.
+
+## **Requirements**
+
+* [Supported Python versions](https://github.com/fivetran/fivetran_connector_sdk/blob/main/README.md#requirements)   
+* Operating System:  
+  * Windows 10 or later  
+  * macOS 13 (Ventura) or later
+
+## **Getting Started**
+
+Refer to the [Setup Guide](https://fivetran.com/docs/connectors/connector-sdk/setup-guide) to get started.
+
+## **Features**
+
+- Connects to ClickHouse cloud or on-premise instances
+- Streams query results to handle large datasets efficiently
+- Tracks sync progress using state and checkpointing
+- Supports incremental syncs
+
+## **Configuration File**
+
+The connector requires the following configuration parameters: 
+
+```
+{
+  "hostname": "<YOUR_CLICKHOUSE_HOSTNAME>",
+  "username": "<YOUR_CLICKHOUSE_USERNAME>",
+  "password": "<YOUR_CLICKHOUSE_PASSWORD>",
+  "database": "<YOUR_CLICKHOUSE_DATABASE>"
+}
+```
+
+- hostname: Your ClickHouse server hostname
+- username: Username for authentication
+- password: Password for authentication
+- database: The ClickHouse database to connect to
+
+Note: Ensure that the `configuration.json` file is not checked into version control to protect sensitive information.
+
+## **Requirements File**
+
+The connector requires the clickhouse_connect Python library for connecting to ClickHouse:
+
+```
+clickhouse_connect==0.8.17
+```
+
+Note: The `fivetran_connector_sdk:latest` and `requests:latest` packages are pre-installed in the Fivetran environment. To avoid dependency conflicts, do not declare them in your `requirements.txt`.
+
+## **Authentication**
+
+The connector uses basic username and password authentication to connect to ClickHouse. These credentials are specified in the configuration file. The connector uses TLS/SSL for secure connections (indicated by the secure=True parameter in the client configuration).  To obtain credentials: 
+
+1. Create an account in the ClickHouse Cloud console
+2. Click on the "Connect" button for your cluster
+3. Choose the python Language client to get the connection details
+4. Use the connection details to fill in the configuration file
+
+## **Pagination**
+
+The connector uses ClickHouse's streaming query capabilities to efficiently process large datasets without loading all data into memory. This is implemented using the `client.query_rows_stream()` method which yields rows as they are retrieved from the database. 
+
+## **Data Handling**
+
+The connector:  
+- Establishes a connection to the ClickHouse database.
+- Inserts data into dummy table for testing purposes.
+- Retrieves column metadata for the specified table.
+- Executes queries with incremental filtering based on a timestamp column (`created_at`).
+- Upserts data into destination table.
+
+## **Error Handling**
+
+The connector implements error handling for:  
+- Connection failures: Raises informative exceptions if the connector fails to connect to ClickHouse
+- Configuration validation: Checks for required configuration parameters before attempting to connect
+
+## **Additional Considerations**
+
+The examples provided are intended to help you effectively use Fivetran's Connector SDK. While we've tested the code, Fivetran cannot be held responsible for any unexpected or negative consequences that may arise from using these examples. For inquiries, please reach out to our Support team.
---
+++ examples/source_examples/clickhouse/connector.py
@@ -0,0 +1,201 @@
+# This is an example for how to work with the fivetran_connector_sdk module.
+# It defines a simple 'update' method, which upserts data from Clickhouse database.
+# See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
+# and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details
+
+# Import the required classes from the connector SDK
+from fivetran_connector_sdk import Connector
+from fivetran_connector_sdk import Operations as op
+from fivetran_connector_sdk import Logging as log
+
+# Import the required libraries
+import datetime
+import random
+import json
+import clickhouse_connect # This is used to connect to ClickHouse
+
+
+def create_clickhouse_client(configuration: dict):
+    """
+    Create and test a connection to ClickHouse
+    This function creates a ClickHouse client using the clickhouse_connect library.
+    Args:
+        configuration: a dictionary that holds the configuration settings for the connector.
+    Returns:
+        client: a ClickHouse client object
+    """
+    client = clickhouse_connect.get_client(
+        host= configuration.get("hostname"),
+        username=configuration.get("username"),
+        password=configuration.get("password"),
+        secure=True
+    )
+
+    try:
+        test_connection = client.query("SELECT 1").result_rows[0][0]
+        log.info(f"Connection to ClickHouse successful: {test_connection}")
+        return client
+    except Exception as e:
+        raise RuntimeError(f"Failed to connect to ClickHouse: {e}")
+
+
+def create_dummy_table(client, database_name, table_name):
+    """
+    Create a dummy database and dummy table in ClickHouse
+    This method is used for testing purposes.
+    In production, you would typically not create a database or table.
+    Args:
+        client: a ClickHouse client object
+        database_name: the name of the database to create
+        table_name: the name of the table to create
+    Returns:
+        database_name: the name of the created database
+        table_name: the name of the created table
+    """
+    # Create a dummy database
+    client.command(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+    log.info(f"Database '{database_name}' created or already exists")
+
+    # Create a table
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {database_name}.{table_name} (
+        id UInt32,
+        name String,
+        value Float64,
+        created_at DateTime
+    ) ENGINE = MergeTree() ORDER BY id
+    """
+    client.command(create_table_query)
+    log.info(f"Table '{database_name}.{table_name}' created")
+
+
+def insert_dummy_data(client, database_name, table_name):
+    """
+    Insert dummy data into the specified table
+    This method is used for testing purposes.
+    In production, you would typically not insert dummy data.
+    Args:
+        client: a ClickHouse client object
+        database_name: the name of the database
+        table_name: the name of the table
+    """
+    # Create the dummy database and table
+    create_dummy_table(client, database_name, table_name)
+
+    data = []
+    # Inserting 10000 rows of data in dummy table
+    for index in range(1000):
+        data.append([
+            index,
+            f"item_{index}",
+            round(random.uniform(1.0, 1000.0), 2),
+            (datetime.datetime.now() - datetime.timedelta(days=random.randint(0, 30)))
+        ])
+
+    client.insert(f"{database_name}.{table_name}", data, column_names=["id", "name", "value", "created_at"])
+    log.info(f"Inserted {len(data)} records in {database_name}.{table_name} successfully.")
+
+
+def execute_query_and_upsert(client, query, table_name, state):
+    """
+    This function executes a query and upserts the results into the ClickHouse database.
+    The data is fetched in a streaming manner to handle large datasets efficiently.
+    Args:
+        client: a ClickHouse client object
+        query: the SQL query to execute
+        table_name: the name of the table to upsert data into
+        state: a dictionary containing state information from previous runs
+    """
+    # This query fetches the column names from the ClickHouse table
+    # The column_names is not supported in ClickHouse streaming queries
+    # So we need to run a separate query to get the column names
+    # This is needed to map the data to the correct columns in the upsert operation
+    column_names = client.query(query + " LIMIT 0").column_names
+
+    with client.query_rows_stream(query) as stream:
+        for row in stream:
+            # Convert the row tuple to a dictionary using the column names
+            row = dict(zip(column_names, row))
+            # Upsert the data into the destination table
+            yield op.upsert(table=table_name, data=row)
+
+            # Update the state with the last created_at timestamp
+            if row['created_at']:
+                last_created = row.get("created_at").isoformat()
+                if last_created > state.get("last_created", "01-01-1800T00:00:00"):
+                    state["last_created"] = last_created
+
+    log.info(f"Upserted data into {table_name} successfully.")
+    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
+    # from the correct position in case of next sync or interruptions.
+    # Learn more about how and where to checkpoint by reading our best practices documentation
+    # (https://fivetran.com/docs/connectors/connector-sdk/best-practices#largedatasetrecommendation).
+    yield op.checkpoint(state)
+
+
+def schema(configuration: dict):
+    """
+    Define the schema function which lets you configure the schema your connector delivers.
+    See the technical reference documentation for more details on the schema function:
+    https://fivetran.com/docs/connectors/connector-sdk/technical-reference#schema
+    Args:
+        configuration: a dictionary that holds the configuration settings for the connector.
+    """
+
+    # check if required configuration values are present in configuration
+    for key in ['hostname', 'username', 'password', 'database']:
+        if key not in configuration:
+            raise ValueError(f"Missing required configuration value : {key}")
+
+    return [
+        {
+            "table": "test_table",
+            "primary_key": ["id"],
+            "columns": {
+                "id": "INT",
+                "name": "STRING",
+                "value": "FLOAT",
+                "created_at": "UTC_DATETIME",
+            },
+        }
+    ]
+
+
+def update(configuration, state):
+    """
+    Define the update function, which is a required function, and is called by Fivetran during each sync.
+    See the technical reference documentation for more details on the update function
+    https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update
+    Args:
+        configuration: A dictionary containing connection details
+        state: A dictionary containing state information from previous runs
+        The state dictionary is empty for the first sync or for any full re-sync
+    """
+    # Create a ClickHouse client
+    client = create_clickhouse_client(configuration)
+    database_name = configuration.get("database")
+    table_name = "sample_table"
+
+    # Insert dummy data for testing purposes
+    # This method is used only for testing purposes and will not be used in production.
+    insert_dummy_data(client=client, database_name=database_name, table_name=table_name)
+
+    last_created = state.get("last_created", "1990-01-01T00:00:00")
+
+    # Execute the query to fetch data from the ClickHouse table and upsert it
+    clickhouse_query = f"SELECT * FROM {database_name}.{table_name} WHERE created_at > '{last_created}'"
+    # The name of the table to upsert data into
+    destination_table = "destination_table"
+    yield from execute_query_and_upsert(client=client, query=clickhouse_query, table_name=destination_table, state=state)
+
+
+# Create the connector object using the schema and update functions
+connector = Connector(update=update, schema=schema)
+
+if __name__ == "__main__":
+    # Open the configuration.json file and load its contents
+    with open("configuration.json", 'r') as f:
+        configuration = json.load(f)
+
+    # Test the connector locally
+    connector.debug(configuration=configuration)
