e735afaff06281d6174a55014de66fafe9a167e4
Sahil Khirwal <sahil.khirwal@fivetran.com>
2025-05-13 21:17:42 +0530

    pulling from maven central

--- examples/source_examples/ibm_informix/using_jaydebeapi/drivers/installation.sh
+++ examples/source_examples/ibm_informix/using_jaydebeapi/drivers/installation.sh
@@ -9,14 +9,12 @@ echo "Installing JDK..."
 apt-get install -y openjdk-17-jre
 echo "JRE 17 installed successfully."
 
-# 2. Download JDBC Driver ZIP
-echo "Downloading Informix JDBC driver..."
+echo "Downloading Informix JDBC jar and BSON jar from Maven Central..."
 mkdir -p /opt/informix
-curl -L -o /opt/informix/informixjdbcdriver.zip https://dbschema.com/jdbc-drivers/informixjdbcdriver.zip
+curl -L -o /opt/informix/jdbc-15.0.0.1.1.jar \
+  https://repo1.maven.org/maven2/com/ibm/informix/jdbc/15.0.0.1.1/jdbc-15.0.0.1.1.jar
 
-# 3. Unzip the JDBC driver
-echo "Unzipping JDBC driver..."
-unzip /opt/informix/informixjdbcdriver.zip -d /opt/informix
+curl -L -o /opt/informix/bson-3.8.0.jar \
+  https://repo1.maven.org/maven2/org/mongodb/bson/5.5.0/bson-3.8.0.jar
 
-echo "JDBC driver unzipped to /opt/informix"
 echo "Installation of Informix JDBC driver complete."
