2686518ea032aa298cde933ec46ac1c2f35feb7c
Satvik Patil <satvik.patil@satvik.patil-W4LTPGX1Q0>
2025-06-08 16:13:40 +0530

    base code

--- examples/common_patterns_for_connectors/ssh_tunnels/connector.py
+++ examples/common_patterns_for_connectors/ssh_tunnels/connector.py
@@ -1,17 +1,83 @@
+# This example demonstrates how to connect to an API over SSH tunnels using `sshtunnel` and `paramiko`.
+# It establishes a secure SSH tunnel from a local port to a remote API server port, allowing secure API access as if it were local.
+# The connector uses an API key to authenticate and retrieve data from the API endpoint over the SSH tunnel.
+# THIS EXAMPLE USES DUMMY DATA AND REQUIRES THE FIVETRAN-API-PLAYGROUND PACKAGE (https://pypi.org/project/fivetran-api-playground/).
+# For this example, an EC2 instance is running fivetran-api-playground and is only accessible via an SSH tunnel.
+# See the Technical Reference documentation
+# (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
+# and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details.
+
+# Import requests to make HTTP calls to API.
 import io
 import json
 from sshtunnel import SSHTunnelForwarder
-import paramiko
+import paramiko # For handling SSH keys and connections
 from fivetran_connector_sdk import Logging as log, Connector  # For enabling Logs in your connector code
 from fivetran_connector_sdk import Operations as op # For supporting Data operations like Upsert(), Update(), Delete() and checkpoint()
+import requests as rq
 
-import requests
-
-REMOTE_PORT = 5000
+REMOTE_PORT = 5005
 LOCAL_PORT = 8000
 
-def update(configuration: dict, state: dict):
-    yield op.upsert(table="hello", data={"message": "hello, world!"})
+# Define the get_auth_headers function, which is your custom function to generate auth headers for making API calls.
+# The function takes one parameter:
+# - config: dictionary contains any secrets or payloads you configure when deploying the connector.
+def get_auth_headers(config):
+    api_key = config.get('api_key')
+
+    if api_key is None:
+        raise ValueError("API Key is missing in the configuration.")
+
+    # Create the auth string
+    headers = {
+        "Authorization": f"apiKey {api_key}",
+        "Content-Type": "application/json",  # Optional: specify content type
+    }
+    return headers
+
+# The sync_items function handles the retrieval of data.
+# It performs the following tasks:
+# 1. Sends an API request to the specified URL with the provided parameters.
+# 2. Processes the items returned in the API response by yielding upsert operations to Fivetran.
+# 3. Saves the state periodically to ensure the sync can resume from the correct point.
+#
+# The function takes three parameters:
+# - base_url: The URL to the API endpoint.
+# - params: A dictionary of query parameters to be sent with the API request.
+# - state: A dictionary representing the current state of the sync, including the last retrieved key.
+# - configuration: A dictionary contains any secrets or payloads you configure when deploying the connector.
+def sync_items(params, state, configuration):
+    response_page = get_api_response(params, get_auth_headers(configuration), configuration)
+
+    # Process the items.
+    items = response_page.get("data", [])
+    if not items:
+        return
+
+    # Iterate over each user in the 'items' list and yield an upsert operation.
+    # The 'upsert' operation inserts the data into the destination.
+    for user in items:
+        yield op.upsert(table="user", data=user)
+
+    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
+    # from the correct position in case of next sync or interruptions.
+    # Learn more about how and where to checkpoint by reading our best practices documentation
+    # (https://fivetran.com/docs/connectors/connector-sdk/best-practices#largedatasetrecommendation).
+    yield op.checkpoint(state)
+
+# The get_api_response function sends an HTTP GET request to the provided URL with the specified parameters.
+# It performs the following tasks:
+# 1. Logs the URL and query parameters used for the API call for debugging and tracking purposes.
+# 2. Makes the API request using the 'requests' library, passing the URL and parameters.
+# 3. Parses the JSON response from the API and returns it as a dictionary.
+#
+# The function takes two parameters:
+# - base_url: The URL to which the API request is made.
+# - params: A dictionary of query parameters to be included in the API request.
+#
+# Returns:
+# - response_page: A dictionary containing the parsed JSON response from the API.
+def get_api_response(params, headers, configuration):
     ssh_host = configuration.get("ssh_host")
     ssh_user = configuration.get("ssh_user")
     private_key_string = configuration.get("private_key")
@@ -28,18 +94,29 @@ def update(configuration: dict, state: dict):
         log.severe(f"Tunnel open at http://127.0.0.1:{LOCAL_PORT}")
 
         # Insert data
-        add_resp = requests.post(
-            f"http://127.0.0.1:{LOCAL_PORT}/add",
-            json={"name": "Test Item"}
-        )
-        log.severe("Add response:", add_resp.json())
+        base_url = f"http://127.0.0.1:{LOCAL_PORT}/auth/api_key"
+        response = rq.get(base_url, params=params, headers=headers)
+        response.raise_for_status()  # Ensure we raise an exception for HTTP errors.
+        response_page = response.json()
+        return response_page
 
-        # Fetch data
-        fetch_resp = requests.get(f"http://127.0.0.1:{LOCAL_PORT}/fetch")
-        log.severe("Fetch response:", fetch_resp.json())
+# Define the update function, which is a required function, and is called by Fivetran during each sync.
+# See the technical reference documentation for more details on the update function
+# https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update
+# The function takes two parameters:
+# - configuration: dictionary contains any secrets or payloads you configure when deploying the connector.
+# - state: a dictionary that contains whatever state you have chosen to checkpoint during the prior sync.
+# The state dictionary is empty for the first sync or for any full re-sync.
+def update(configuration: dict, state: dict):
+    yield from sync_items({}, state, configuration)
 
+# This creates the connector object that will use the update and schema functions defined in this connector.py file.
 connector = Connector(update=update)
 
+# Check if the script is being run as the main module. This is Python's standard entry method allowing your script to
+# be run directly from the command line or IDE 'run' button. This is useful for debugging while you write your code.
+# Note this method is not called by Fivetran when executing your connector in production. Please test using the
+# Fivetran debug command prior to finalizing and deploying your connector.
 if __name__ == "__main__":
     try:
         with open("configuration.json", 'r') as f:
