a911e6875073fe259c7c695ce53b9d32e6a8b036
anushka.parashar <anushka.parashar@fivetran.com>
2025-07-16 23:07:02 +0530

    Added state update

--- examples/source_examples/couchbase_magma/connector.py
+++ examples/source_examples/couchbase_magma/connector.py
@@ -1,7 +1,14 @@
+# This is an example for how to work with the fivetran_connector_sdk module.
+# This example demonstrates how to work with a Couchbase Capella.
+# See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
+# and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details
+
+# Import the required classes from the connector SDK
 from fivetran_connector_sdk import Connector
 from fivetran_connector_sdk import Operations as op
 from fivetran_connector_sdk import Logging as log
 
+# Import the required libraries
 from datetime import timedelta
 import json
 from couchbase.auth import PasswordAuthenticator
@@ -12,16 +19,21 @@ def create_couchbase_client(configuration: dict):
     """
     Create a Couchbase client for on-prem or cloud-hosted Couchbase Server.
     Supports both TLS and non-TLS endpoints based on configuration.
+    Args:
+        configuration: a dictionary that holds the configuration settings for the connector.
+    Returns:
+        cluster_bucket: a couchbase bucket reference object
     """
     username = configuration.get("username")
     password = configuration.get("password")
-    endpoint = configuration.get("endpoint")          # e.g. "localhost", "192.168.0.100"
+    endpoint = configuration.get("endpoint")
     bucket_name = configuration.get("bucket_name")
     use_tls_str = configuration.get("use_tls", "false").strip().lower()     # Optional flag to use TLS
     use_tls = use_tls_str == "true"
-    cert_path = configuration.get("cert_path")        # Optional path to CA cert
+    cert_path = configuration.get("cert_path")        # Optional path to cert
 
     try:
+        # Authenticate using the provided username and password
         auth = PasswordAuthenticator(username, password)
         options = ClusterOptions(auth)
 
@@ -35,7 +47,10 @@ def create_couchbase_client(configuration: dict):
             connection_string = f"couchbase://{endpoint}"
             cluster = Cluster(connection_string, options)
 
+        # Wait until the cluster is ready for use.
         cluster.wait_until_ready(timedelta(seconds=5))
+
+        # get a reference to couchbase magma bucket
         cluster_bucket = cluster.bucket(bucket_name)
         log.info(f"Connected to bucket '{bucket_name}' at '{connection_string}' successfully.")
         return cluster_bucket
@@ -45,27 +60,63 @@ def create_couchbase_client(configuration: dict):
 
 
 def execute_query_and_upsert(client, scope, collection, query, table_name, state):
+    """
+        This function executes a query and upserts the results into destination.
+        The data is fetched in a streaming manner to handle large datasets efficiently.
+        Args:
+            client: a couchbase client object
+            scope: the scope of the collection
+            collection: the name of the collection to query
+            query: the SQL query to execute
+            table_name: the name of the table to upsert data into
+            state: a dictionary that holds the state of the connector
+        """
+    # set the scope in the couchbase client
     client_scope = client.scope(scope)
     count = 0
     checkpoint_interval = 1000
 
     try:
+        # Execute the query and fetch the results
+        # By default, couchbase does not load the entire data in the memory.
+        # Instead, it streams the data when iterated over. This helps to avoid memory overflow issues.
         row_iter = client_scope.query(query, QueryOptions(metrics=False))
         for row in row_iter:
             row_data = row.get(collection)
+            # The yield statement returns a generator object.
+            # This generator will yield an upsert operation to the Fivetran connector.
+            # The op.upsert method is called with two arguments:
+            # - The first argument is the name of the table to upsert the data into.
+            # - The second argument is a dictionary containing the data to be upserted.
             yield op.upsert(table=table_name, data=row_data)
+            # Update state based on the latest "created_at"
+            state["last_created_at"] = row_data["created_at"]
             count += 1
 
             if count % checkpoint_interval == 0:
+                # Checkpoint the state every CHECKPOINT_INTERVAL records to avoid losing progress
+                # With regular checkpointing, the next sync will start from the last checkpoint of the previous failed sync, thus saving time.
                 yield op.checkpoint(state)
 
+        yield op.checkpoint(state)
+
     except Exception as e:
+        # In case of exception, raise a RuntimeError
         raise RuntimeError(f"Failed to fetch and upsert data: {e}")
 
     log.info(f"Upserted {count} records into {table_name} successfully.")
 
 
 def schema(configuration: dict):
+    """
+        Define the schema function which lets you configure the schema your connector delivers.
+        See the technical reference documentation for more details on the schema function:
+        https://fivetran.com/docs/connectors/connector-sdk/technical-reference#schema
+        Args:
+            configuration: a dictionary that holds the configuration settings for the connector.
+        """
+
+    # check if required configuration values are present in configuration
     for key in ["endpoint", "username", "password", "bucket_name", "scope", "collection"]:
         if key not in configuration:
             raise ValueError(f"Missing required configuration value: {key}")
@@ -81,19 +132,41 @@ def schema(configuration: dict):
                 "type": "STRING",
                 "callsign": "STRING",
                 "iata": "STRING",
-                "icao": "STRING"
+                "icao": "STRING",
+                "created_at": "STRING"
             },
         }
     ]
 
 
 def update(configuration, state):
+    """
+        Define the update function, which is a required function, and is called by Fivetran during each sync.
+        See the technical reference documentation for more details on the update function
+        https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update
+        Args:
+            configuration: A dictionary containing connection details
+            state: A dictionary containing state information from previous runs
+            The state dictionary is empty for the first sync or for any full re-sync
+        """
+    # Create a couchbase client
     client = create_couchbase_client(configuration)
     scope = configuration.get("scope")
     collection = configuration.get("collection")
-    table_name = "airline_table"
-    sql_query = f"SELECT * FROM `{collection}`"
+    last_created = state.get("last_created_at", "1970-01-01T00:00:00Z")
 
+    # name of the table to upsert data into
+    table_name = "airline_table"
+
+    # SQL query to fetch data from the collection
+    # You can modify this query to fetch data as per your requirements.
+    sql_query = (
+        f"SELECT * FROM `{collection}` "
+        f"WHERE created_at > '{last_created}' "
+        f"ORDER BY created_at ASC"
+    )
+
+    # execute the query and upsert the data into destination
     yield from execute_query_and_upsert(
         client=client,
         scope=scope,
@@ -103,12 +176,20 @@ def update(configuration, state):
         state=state,
     )
 
+    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
+    # from the correct position in case of next sync or interruptions.
+    # Learn more about how and where to checkpoint by reading our best practices documentation
+    # (https://fivetran.com/docs/connectors/connector-sdk/best-practices#largedatasetrecommendation).
     yield op.checkpoint(state)
 
 
+# Create the connector object using the schema and update functions
 connector = Connector(update=update, schema=schema)
 
 if __name__ == "__main__":
+    # Open the configuration.json file and load its contents
     with open("configuration.json", "r") as f:
         configuration = json.load(f)
+
+    # Test the connector locally
     connector.debug(configuration=configuration)
