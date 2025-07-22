63366f745a87c91630cb5150789a2c16c8cf1389
fivetran-kirkvanarkel <108479451+fivetran-kirkvanarkel@users.noreply.github.com>
2025-07-22 15:36:40 -0700

    adding Firebird DB example (#180)
    * adding Firebird DB example
    
    * Create requirements.txt
    
    * Update README.md
    
    * Update connector.py
    
    removing misleading comment
    
    * Update examples/source_examples/firebird_db/README.md
    
    ---------
    
    Co-authored-by: Chloe Peterson-Cochrane <54036902+fivetran-chloe@users.noreply.github.com>

---
+++ examples/source_examples/firebird_db/schema.py
@@ -0,0 +1,17 @@
+table_list = [
+    {
+        "table_name": "table_1", 
+        "incremental_column": "ID",
+        "primary_key": ["ID"]
+    },
+    {
+        "table_name": "table_2", 
+        "incremental_column": "DATE_MODIFIED",
+        "primary_key": ["ID"]
+    },
+    {
+        "table_name": "table_3", 
+        "incremental_column": "DATE_MODIFIED",
+        "primary_key": ["ID"]
+    }
+]
