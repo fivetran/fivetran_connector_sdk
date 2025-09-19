412b8f3547545fc2db8d1e1e54563fe27c7debb5
Karun Veluru <karunakar.veluru@fivetran.com>
2025-09-19 13:18:16 -0500

    feature(examples): Address PR comments

--- examples/source_examples/adform/connector.py
+++ examples/source_examples/adform/connector.py
@@ -7,7 +7,11 @@ and the Best Practices documentation (https://fivetran.com/docs/connectors/conne
 
 # Import required classes from fivetran_connector_sdk
 from fivetran_connector_sdk import Connector
+
+# For enabling Logs in your connector code
 from fivetran_connector_sdk import Logging as log
+
+# For supporting Data operations like Upsert(), Update(), Delete() and checkpoint()
 from fivetran_connector_sdk import Operations as op
 
 # Import required libraries for API interactions
