551476a4655d2bcd5c8a17d0a29507f124ac6868
fivetran-sumithkumars <97510530+fivetran-sumithkumars@users.noreply.github.com>
2025-09-24 19:00:14 +0530

    Update connectors/checkly/connector.py
    Co-authored-by: Sahil Khirwal <sahil.khirwal@fivetran.com>

--- connectors/checkly/connector.py
+++ connectors/checkly/connector.py
@@ -505,7 +505,7 @@ def update(configuration: dict, state: dict):
         state: A dictionary containing state information from previous runs
         The state dictionary is empty for the first sync or for any full re-sync
     """
-    log.warning("Example: API Connector : Checkly")
+    log.warning("Example: Source Connector : Checkly")
 
     # Validate the configuration to ensure it contains all required values.
     validate_configuration(configuration=configuration)
