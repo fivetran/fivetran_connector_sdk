937eac3c84a9d33bd85a10edaff3d58811a59f63
fivetran-kiranpm <122251000+fivetran-kiranpm@users.noreply.github.com>
2025-10-23 12:25:07 +0530

    Update connectors/npi_registry/connector.py
    Co-authored-by: Sahil Khirwal <sahil.khirwal@fivetran.com>

--- connectors/npi_registry/connector.py
+++ connectors/npi_registry/connector.py
@@ -77,7 +77,7 @@ def update(configuration: dict, state: dict):
         The state dictionary is empty for the first sync or for any full re-sync
     """
 
-    log.warning("Example: API Connector : NPPES NPI Registry Connector")
+    log.warning("Example:Source Examples - NPPES NPI Registry")
 
     # Validate the configuration to ensure it contains all required values.
     validate_configuration(configuration=configuration)
