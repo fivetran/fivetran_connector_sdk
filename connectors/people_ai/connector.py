0631a4dc3ac317815ae263cb1132891eb2d1bb2c
fivetran-adamrees <107201726+fivetran-adamrees@users.noreply.github.com>
2025-11-14 16:48:36 -0700

    Update connector.py

--- connectors/people_ai/connector.py
+++ connectors/people_ai/connector.py
@@ -24,10 +24,13 @@ for implementation guidance.
 
 # For reading configuration from a JSON file
 import json
+
 # Import required classes from fivetran_connector_sdk
 from fivetran_connector_sdk import Connector
+
 # For enabling Logs in your connector code
 from fivetran_connector_sdk import Logging as log
+
 # For supporting Data operations like Upsert(), Update(), Delete() and checkpoint()
 from fivetran_connector_sdk import Operations as op
 
@@ -71,16 +74,16 @@ def schema(configuration: dict) -> List[Dict[str, Any]]:
     https://fivetran.com/docs/connectors/connector-sdk/technical-reference#schema
 
     Args:
-        configuration: a dictionary that holds the configuration settings for the connector.         
+        configuration: a dictionary that holds the configuration settings for the connector.
     """
     return [
         {
             "table": "activity",  # Name of the table in the destination, required.
-            "primary_key": ["uid"], # Primary key column(s) for the table, optional.
+            "primary_key": ["uid"],  # Primary key column(s) for the table, optional.
         },
         {
             "table": "participants",  # Name of the table in the destination, required.
-            "primary_key": ["uid", "email"], # Primary key column(s) for the table, optional.
+            "primary_key": ["uid", "email"],  # Primary key column(s) for the table, optional.
         },
     ]
 
