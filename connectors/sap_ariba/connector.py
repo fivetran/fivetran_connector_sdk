45145ee103fe2a0703aadbc8d5907ab9e18ffeeb
Marisa Smith <66020208+mesmith027@users.noreply.github.com>
2025-12-09 09:55:33 -0400

    Enhance comments on checkpointing in sync process
    Added comments to clarify the importance of checkpointing the state during the sync process.

--- connectors/sap_ariba/connector.py
+++ connectors/sap_ariba/connector.py
@@ -138,7 +138,18 @@ def update(configuration: dict, state: dict):
 
     # Execute sync for both tables.
     sync_orders(params.copy(), headers, state, sync_start)
+    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
+    # from the correct position in case of next sync or interruptions.
+    # Learn more about how and where to checkpoint by reading our best practices documentation
+    # (https://fivetran.com/docs/connectors/connector-sdk/best-practices#largedatasetrecommendation).
+    op.checkpoint(state)
+
     sync_items(params.copy(), headers, state, sync_start)
+    # Save the progress by checkpointing the state. This is important for ensuring that the sync process can resume
+    # from the correct position in case of next sync or interruptions.
+    # Learn more about how and where to checkpoint by reading our best practices documentation
+    # (https://fivetran.com/docs/connectors/connector-sdk/best-practices#largedatasetrecommendation).
+    op.checkpoint(state)
 
     # Final checkpoint after both tables are synced.
     op.checkpoint(state)
