216474181d765cd6916c09827b5dd2688ddc1c89
fivetran-JenasVimal <jenas.vimal@fivetran.com>
2026-02-05 20:46:03 +0530

    Update connectors/google_trends/connector.py
    Co-authored-by: Sahil Khirwal <sahil.khirwal@fivetran.com>

--- connectors/google_trends/connector.py
+++ connectors/google_trends/connector.py
@@ -267,7 +267,7 @@ def update(configuration: dict, state: dict):
         state: A dictionary containing state information from previous runs
         The state dictionary is empty for the first sync or for any full re-sync
     """
-    log.warning("Example: CONNECTORS : GOOGLE_TRENDS")
+    log.warning("Example: Connectors - Google trends example")
 
     # Validate the configuration
     validate_configuration(configuration)
