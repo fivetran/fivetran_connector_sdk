5757ced7a75bf714019b600c947788d873ccbab2
fivetran-meetmahna <meet.mahna@fivetran.com>
2026-05-12 14:24:36 +0530

    Update examples/common_patterns_for_connectors/pagination/database_keyset/connector.py
    Co-authored-by: Jenas Anton Vimal <jenas.vimal@fivetran.com>

--- examples/common_patterns_for_connectors/pagination/database_keyset/connector.py
+++ examples/common_patterns_for_connectors/pagination/database_keyset/connector.py
@@ -215,9 +215,9 @@ def sync_items(connection, table_name, last_updated_at, last_id, state):
 
             for row in rows:
                 # The 'upsert' operation is used to insert or update data in the destination table.
-                # The op.upsert method is called with two arguments:
-                # The first argument is the name of the table to upsert the data into.
-                # The second argument is a dictionary containing the data to be upserted.
+          # The 'upsert' operation is used to insert or update data in the destination table.
+            # The first argument is the name of the destination table.
+            # The second argument is a dictionary containing the record to be upserted.
                 op.upsert(table="user", data=row)
 
             # Advance the keyset boundary to the last row of this page.
